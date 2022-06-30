0f15d139d334290ae54a06d17022539b6c8cc769
AE211115 <thdrkdcks@etoos.com>
2022-06-30 16:43:11 +0900

    initial commit

---
+++ src/main/java/com/example/simplems/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.simplems;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        throw new RuntimeException("error");
+    }
+}
---
+++ src/main/java/com/example/simplems/SimpleMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMsApplication.class, args);
+    }
+
+}
